7ddcf580cd3b29e0c1f2d072fec3a7896fcde413
Julian Bourdes <julian.bourdes@gmail.com>
2018-11-14 11:58:26 +0100

    Update view/produit/list.php

--- view/produit/list.php
+++ view/produit/list.php
@@ -1,7 +1,7 @@
 <?php
     foreach ($tab_p as $p) {
         echo '<a href="index.php?controller=produit&action=read&codeProduit="'.rawurlencode($p->get('codeProduit')).'">
-        
+        <div> <img href="'.File::build_path('Images', $p->get('nomProduit')).'"> '.$p->get('nomProduit').' </div>
         </a>';
     }
 ?>
