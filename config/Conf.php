ff4c10d5b2960f143f0ba5439a48fb6796c7cd1d
Julian Bourdes <julian.bourdes@gmail.com>
2018-11-14 11:45:59 +0100

    Update config/Conf.php

--- config/Conf.php
+++ config/Conf.php
@@ -6,17 +6,17 @@ class Conf {
   static private $databases = array(
     // Hostname is webinfo at IUT
     // or localhost on your computer
-    'hostname' => 'webinfo',
+    'hostname' => 'localhost',
     // At IUT, you have a database named after your login
     // On your computer, please create a database
     'database' => 'bourdesj',
     // At IUT, it is your classical login
     // On your computer, you should have at least a 'root' account
-    'login' => 'bourdesj',
+    'login' => 'root',
     // At IUT, it is your database password 
     // (=PHPMyAdmin pwd, INE by defaut)
     // On your computer, you created the pwd during setup
-    'password' => 'Unicorn'
+    'password' => 'julian'
   );
 
   static private $debug = True;
