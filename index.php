71d003625d66ee0f3179c2fcce8f95f580b0f5b3
Yael Misseri <yael.misseri@etu.umontpellier.fr>
2018-11-14 10:14:41 +0100

    maj

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once __DIR__. DIRECTORY_SEPARATOR. 'lib'. DIRECTORY_SEPARATOR. 'Session.php'; 
-session_start();
-require_once __DIR__ . DIRECTORY_SEPARATOR .'lib'. DIRECTORY_SEPARATOR .'File.php';
-require_once (File::build_path(array('controller', 'routeur.php')));
-
-?>
