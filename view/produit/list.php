1ad1eb7b66b4cdc7c18b1fa8da47e451fbd9fa57
Yael Misseri <yael.misseri@etu.umontpellier.fr>
2018-11-28 12:49:50 +0100

    Fin page list et details et CSS

--- view/produit/list.php
+++ view/produit/list.php
@@ -1,17 +1,31 @@
+
+<div class="panel_admin">
+
+	<a class="bouton" href="index.php?controller=produit&action=create">Ajouter un produit</a>
+
+</div>
+
+<h1> Tous nos produits </h1>
+
+<div class=responsive>
+
+
 <?php
 
-echo '<div class=responsive>';
-
     foreach ($tab_p as $p) {
+
         $img_nom = $p->get('nomProduit').'.png';
         $codeP = $p->get('codeProduit');
         echo '<div class=Produit> <a href="index.php?controller=produit&action=read&codeProduit='.rawurlencode($codeP).'">
          <img src="'.File::build_path(array('Images', $img_nom)).'" > 
         </a>
-        <div class=desc>'.$p->get('nomProduit').' </div>
+        <div class=desc>'.$p->get('nomProduit').' <br> <a>Seulement  ' . $p->get('prixProduit') . '€</a></div>
          </div>';
     }
-echo '</div>';
-echo '<p> <a href="index.php?controller=produit&action=create" style="color:black;text-align:center;"> Cliquez ici pour ajouter un nouveau produit </a> </p>';
 
-?>
+ ?>
+
+
+
+</div>
+
