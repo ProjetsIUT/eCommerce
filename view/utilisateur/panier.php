f00a7089c4bfde1eb378913c0ace6404c492b647
Yael Misseri <yael.misseri@etu.umontpellier.fr>
2018-11-29 16:55:37 +0100

    panier

---
+++ view/utilisateur/panier.php
@@ -0,0 +1,29 @@
+<div id="panier">
+
+	<h1>Mon Panier</h1>
+
+	<div id="liste_articles">
+
+		<?php
+
+    foreach ($tab_p as $p) {
+
+        $img_nom = $p->get('nomProduit').'.png';
+        $codeP = $p->get('codeProduit');
+        echo '<div class=Produit> <a href="index.php?controller=produit&action=read&codeProduit='.rawurlencode($codeP).'">
+         <img src="./Images/'.$img_nom.'" > 
+        </a>
+        <div class=desc>'.$p->get('nomProduit').' <br> <a>Seulement  ' . $p->get('prixProduit') . '€</a></div>
+         </div>';
+    }
+
+ ?>
+
+
+
+	</div>
+
+
+
+
+</div>
