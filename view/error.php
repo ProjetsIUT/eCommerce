9dbda3d215eb0b2ee7716912b72c03220d1e67eb
Julian Bourdès <julian.bourdes@gmail.com>
2018-12-11 08:54:28 +0100

    mise à jour error.php

--- view/error.php
+++ view/error.php
@@ -17,15 +17,33 @@
 
 		<!--Pour l'en-tête de page-->
 		<header>
-		<nav> <!--Menu-->
+		<header>
+			<nav> <!--Menu-->
 				<div class=menu_burger>
 					<a> <img src="./Images/logoananas.png" alt="Ca fonctionne pas nulos" id="logobg"></a>
 					<div class="submenu_bg">
 						<div><a class=txt_sub href="index.php">Accueil</a></div>
 						<br>
 						<div><a class=txt_sub href="index.php?action=readAll&controller=produit">Produits</a></div>
-						<div><a class=txt_sub href="index.php?action=readAll&controller=utilisateur">Utilisateur</a></div>
-						
+						<div><a class=txt_sub href="index.php?action=show_panier&controller=utilisateur">Panier</a></div>
+						<?php
+						if(Session::is_admin()) {
+							echo '<div><a class=txt_sub href="index.php?action=readAll&controller=utilisateur">Utilisateurs</a></div>
+							<div><a class=txt_sub href="index.php?action=readAll&controller=commande">Historique des commandes</a></div>';
+						}
+						else if (isset($_SESSION['loginUtilisateur'])){
+							echo '<div><a class=txt_sub href="index.php?action=read&controller=utilisateur&loginUtilisateur='.$_SESSION['loginUtilisateur'].'">Mon Compte</a></div>
+							<div><a class=txt_sub href="index.php?action=readAll&controller=commande">Historique des commandes</a></div>';
+						}
+
+                    	if(isset($_SESSION['loginUtilisateur'])) {
+							echo ('<div><a class=txt_sub href="index.php?controller=utilisateur&action=deconnect">Se déconnecter</a></div>');
+                    	}
+                    	else {
+							echo ('<div><a class=txt_sub href="index.php?action=create&controller=utilisateur">S\'inscrire</a></div>
+							<div><a class=txt_sub href="index.php?action=connect&controller=utilisateur">Se connecter</a></div>');
+                    	}
+                    	?>
 					</div>
 				</div>
 			
@@ -33,8 +51,29 @@
 				<div class=menu>
 					<a href="index.php"><img src="./Images/logoananas.png" alt="Ca fonctionne pas nulos" id="logo"></a>			
 					<div class=title_menu><a class=txt_menu href="index.php?action=readAll&controller=produit">Produits</a></div>
-					<div class=title_menu><a class=txt_menu href="index.php?action=readAll&controller=utilisateur">Utilisateur</a></div>
+					<div class=title_menu><a class=txt_menu href="index.php?action=show_panier&controller=utilisateur">Panier</a></div>
+					<?php
+					if(Session::is_admin()) {
+						echo '<div class=title_menu><a class=txt_menu href="index.php?action=readAll&controller=utilisateur">Utilisateurs</a></div>
+						<div class=title_menu><a class=txt_menu href="index.php?action=readAll&controller=commande">Historique des commandes</a></div>
+';
+					}
+					else if(isset($_SESSION['loginUtilisateur'])){
+						echo '<div class=title_menu><a class=txt_menu href="index.php?action=read&controller=utilisateur&loginUtilisateur='.$_SESSION['loginUtilisateur'].'">Mon Compte</a></div>
+						<div class=title_menu><a class=txt_menu href="index.php?action=readAll&controller=commande">Historique des commandes</a></div>
+';
+					}
+
+					if(isset($_SESSION['loginUtilisateur'])) {
+						echo ('<div class=title_menu><a class=txt_menu href="index.php?controller=utilisateur&action=deconnect">Se déconnecter</a></div>');
+					}
+					else {
+						echo ('<div class=title_menu><a class=txt_menu href="index.php?action=create&controller=utilisateur">S\'inscrire</a></div>
+						<div class=title_menu><a class=txt_menu href="index.php?action=connect&controller=utilisateur">Se connecter</a></div>');
+					}
+					?>
 				</div>
+			
 			</nav>
 		</header>
     <body>
