13d1a07aa45b3ff6a384fe19a6c37b6872e3122f
Julian Bourdes <julian.bourdes@gmail.com>
2018-12-11 14:26:49 +0100

    Update panier.php

--- view/utilisateur/panier.php
+++ view/utilisateur/panier.php
@@ -77,6 +77,7 @@
             <strong class="total">Total: '. $montant_total . '€</strong>
 
             <br>
+            <a class="bouton" href="./">Continuer vos achats sans plus attendre !</a>
             <a class="bouton_panier" href="./index.php?controller=commande&action=passerCommande&montant='. $montant_total .'">Passer la commande</a>
             ';
 
